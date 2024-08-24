e92e9d60fdd91806610a083c16f5e3a98b0a2425
Taz <john.herrman@gmail.com>
2024-08-24 17:07:24 -0500

    Additional comments

--- Day15Project.sql
+++ Day15Project.sql
@@ -1,3 +1,9 @@
+/*
+ This is the SQL script for the Udemy SQL course.
+ Going to use this script to also practice some git and github commands.
+ */
+
+--Task 1.1
 create table employees (
     emp_id int PRIMARY KEY ,
     first_name text not NULL ,
@@ -11,12 +17,14 @@ create table employees (
     manager_id int
 );
 
+--task 1.2
 create table departments (
     department_id int PRIMARY KEY ,
     department text not null,
     division text not null
 );
 
+--task 2
 alter table if exists employees
     alter department_id set not null,
     alter start_date set DEFAULT current_date,
